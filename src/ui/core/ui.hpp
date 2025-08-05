0114304607ec3c0381427b02d362eaa38a049b00
levalyukov <lev.alyukov@gmail.com>
2025-08-05 14:29:25 +0500

    add transform lowercase of name ui element

--- src/ui/core/ui.hpp
+++ src/ui/core/ui.hpp
@@ -4,24 +4,30 @@
 #include "elements/ui-element.hpp"
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 class UIManager {
 	public:
 		static UIManager& Instance() {
 			static UIManager r;
 			return r;
-		}
+		};
 
 		inline void addElement(std::string name_element, std::unique_ptr<UIElement> element) {
+			std::transform(name_element.begin(), name_element.end(), name_element.begin(), [](unsigned char c) { return std::tolower(c); });
 			ui_elements.emplace(name_element, std::move(element));
 		};
 
 		inline UIElement* getElement(std::string name_element) {
+			std::transform(name_element.begin(), name_element.end(), name_element.begin(), [](unsigned char c) { return std::tolower(c); });
 			auto element = ui_elements.find(name_element);
 			return (element != ui_elements.end()) ? element->second.get() : nullptr;
 		};
 
 		inline void removeElement(std::string name_element) {
+			std::transform(name_element.begin(), name_element.end(), name_element.begin(), [](unsigned char c) { return std::tolower(c); });
 			auto element = ui_elements.find(name_element);
 			if (element != ui_elements.end()) {
 				ui_elements.erase(name_element);
@@ -31,7 +37,9 @@ class UIManager {
 		inline void render(sf::Event& event, sf::RenderWindow& window) {
 			for (const auto& element : ui_elements) {
 				element.second->handleEvent(event, window);
-				element.second->draw(window);
+				if (element.second->getVisible()) {
+					element.second->render(window);
+				}
 			}
 		};
 
@@ -40,7 +48,6 @@ class UIManager {
 	private:
 		UIManager() {};
 		~UIManager() {};
-
 		UIManager(UIManager const&) = delete;
 		UIManager& operator=(UIManager const&) = delete;
 
