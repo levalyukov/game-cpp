1081eeec4f29aa6af90922b0c2f2857bb8b85fd1
levalyukov <lev.alyukov@gmail.com>
2025-08-07 00:31:40 +0500

    Update states.hpp

--- src/core/states.hpp
+++ src/core/states.hpp
@@ -1,8 +1,3 @@
-/*
-	sdasdas
-	asda
-*/
-
 #pragma once
 
 class GameState {
@@ -14,14 +9,21 @@ class GameState {
 
 		inline void setGamePause(bool state) { paused = state; };
 		inline bool getGamePause() const { return paused; };
-		inline void setWindowStatus(bool state) { window = state; };
-		inline bool getWindowStatus() const { return window; };
+		inline void setWindowStatus(bool state) { windowOpen = state; };
+		inline bool getWindowStatus() const { return windowOpen; };
+		inline sf::RenderWindow& getGameWindow() const { return *window; };
+		inline sf::Clock* getGameClock() const { return clock.get(); };
 
 	private:
-		bool paused;
-		bool window = true;
+		bool paused = false;
+		bool windowOpen = true;
+		std::unique_ptr<sf::RenderWindow> window;
+		std::unique_ptr<sf::Clock> clock;
 
-		GameState() {};
+		GameState() {
+			window = std::make_unique<sf::RenderWindow>(sf::VideoMode({ 1280, 720 }), "WINDOW_TITLE");
+			clock = std::make_unique<sf::Clock>();
+		};
 		~GameState() {};
 		GameState(GameState& const) = delete;
 		GameState& operator=(GameState& const) = delete;
