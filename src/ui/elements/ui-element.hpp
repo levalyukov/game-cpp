e8494d71724a807d1bdce31951c0b836e21e7743
levalyukov <lev.alyukov@gmail.com>
2025-08-12 20:20:44 +0500

    add: setter global position for ui elements

--- src/ui/elements/ui-element.hpp
+++ src/ui/elements/ui-element.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "../../core/globals.hpp"
+
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Keyboard.hpp>
+
+#include <iostream>
 #include <functional>
 
 class UIElement {
@@ -12,14 +16,45 @@ class UIElement {
 			BottomLeft, BottomCenter, BottomRight
 		};
 
-		//virtual void setElementPosition(sf::RenderWindow& window, ElementPosition current_position) {
-		//	switch (current_position) {
-		//		case TopLeft:
-		//			break;
-		//		case TopCenter:
+		void setGlobalPosition(ElementPosition current_position, sf::Sprite& sprite) {
+			sf::RenderWindow& window = Globals::instance().getWindow();
+			float windowSizeX = static_cast<float>(window.getSize().x);
+			float windowSizeY = static_cast<float>(window.getSize().y);
+			float spriteSizeWidth = static_cast<float>(sprite.getGlobalBounds().width);
+			float spriteSizeHeight = static_cast<float>(sprite.getGlobalBounds().height);
 
-		//	}
-		//};
+			switch (current_position) {
+				case TopLeft:
+					sprite.setPosition({ 0,0 });
+					break;
+				case TopCenter:
+					sprite.setPosition({ (windowSizeX - spriteSizeWidth) / 2.f, 0 });
+					break;
+				case TopRight:
+					sprite.setPosition({ windowSizeX - spriteSizeWidth, 0 });
+					break;
+
+				case MiddleLeft:
+					sprite.setPosition({ 0, (windowSizeY - spriteSizeHeight) / 2.f });
+					break;
+				case MiddleCenter:
+					sprite.setPosition({ (windowSizeX - spriteSizeWidth) / 2.f, (windowSizeY - spriteSizeHeight) / 2.f });
+					break;
+				case MiddleRight:
+					sprite.setPosition({ windowSizeX - spriteSizeWidth, (windowSizeY - spriteSizeHeight) / 2.f });
+					break;
+
+				case BottomLeft:
+					sprite.setPosition({ 0, windowSizeY - spriteSizeHeight });
+					break;
+				case BottomCenter:
+					sprite.setPosition({ (windowSizeX - spriteSizeWidth) / 2.f, windowSizeY - spriteSizeHeight });
+					break;
+				case BottomRight:
+					sprite.setPosition({ windowSizeX - spriteSizeWidth, windowSizeY - spriteSizeHeight });
+					break;
+			}
+		};
 
 		virtual void setVisible(bool state) = 0;
 		virtual bool getVisible() const = 0;
