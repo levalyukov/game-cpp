3b997c20a8378408c34bf5fae73b0edffc3695aa
levalyukov <lev.alyukov@gmail.com>
2025-08-14 19:57:52 +0500

    transferring the implementation of functions to a separate file

--- src/ui/elements/ui-element.hpp
+++ src/ui/elements/ui-element.hpp
@@ -3,7 +3,6 @@
 #include "../../core/globals.hpp"
 
 #include <SFML/Graphics.hpp>
-#include <SFML/Window/Keyboard.hpp>
 
 #include <iostream>
 #include <functional>
@@ -16,45 +15,8 @@ class UIElement {
 			BottomLeft, BottomCenter, BottomRight
 		};
 
-		void setGlobalPosition(ElementPosition current_position, sf::Sprite& sprite) {
-			sf::RenderWindow& window = Globals::instance().getWindow();
-			float windowSizeX = static_cast<float>(window.getSize().x);
-			float windowSizeY = static_cast<float>(window.getSize().y);
-			float spriteSizeWidth = static_cast<float>(sprite.getGlobalBounds().width);
-			float spriteSizeHeight = static_cast<float>(sprite.getGlobalBounds().height);
-
-			switch (current_position) {
-				case TopLeft:
-					sprite.setPosition({ 0,0 });
-					break;
-				case TopCenter:
-					sprite.setPosition({ (windowSizeX - spriteSizeWidth) / 2.f, 0 });
-					break;
-				case TopRight:
-					sprite.setPosition({ windowSizeX - spriteSizeWidth, 0 });
-					break;
-
-				case MiddleLeft:
-					sprite.setPosition({ 0, (windowSizeY - spriteSizeHeight) / 2.f });
-					break;
-				case MiddleCenter:
-					sprite.setPosition({ (windowSizeX - spriteSizeWidth) / 2.f, (windowSizeY - spriteSizeHeight) / 2.f });
-					break;
-				case MiddleRight:
-					sprite.setPosition({ windowSizeX - spriteSizeWidth, (windowSizeY - spriteSizeHeight) / 2.f });
-					break;
-
-				case BottomLeft:
-					sprite.setPosition({ 0, windowSizeY - spriteSizeHeight });
-					break;
-				case BottomCenter:
-					sprite.setPosition({ (windowSizeX - spriteSizeWidth) / 2.f, windowSizeY - spriteSizeHeight });
-					break;
-				case BottomRight:
-					sprite.setPosition({ windowSizeX - spriteSizeWidth, windowSizeY - spriteSizeHeight });
-					break;
-			}
-		};
+		void setGlobalPosition(enum ElementPosition current_position, sf::Sprite& sprite);
+		void setRelativePosition(enum ElementPosition current_position, sf::Sprite& parent_sprite, sf::Sprite& child_sprite);
 
 		virtual void setSortIndex(unsigned __int8 new_z_index) = 0;
 		virtual unsigned __int8 getSortIndex() const = 0;
