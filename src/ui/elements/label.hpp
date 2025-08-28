355c1a6c01b5648075b61aec73a815c206b3042f
levalyukov <lev.alyukov@gmail.com>
2025-08-28 11:57:51 +0500

    remove unnecessary parameters in label

--- src/ui/elements/label.hpp
+++ src/ui/elements/label.hpp
@@ -10,37 +10,32 @@ class Label : public UIElement {
 			unsigned __int8 size,
 			sf::Color color,
 			sf::Vector2f coords_position
-		) : message(message),
-			font(*font),
-			size(size),
-			color(color) {
-			text.setFont(*font);
-			text.setCharacterSize(size);
-			text.setColor(color);
-			text.setString(message);
-			text.setPosition(coords_position);
+		) : font(*font) {
+			text->setFont(*font);
+			text->setCharacterSize(size);
+			text->setColor(color);
+			text->setString(message);
+			text->setPosition(coords_position);
 		};
 
 		std::function<void()> handler;
 
-		inline void setText(std::string new_msg) { message = new_msg; text.setString(new_msg); };
-		inline void setColor(sf::Color new_color) { color = new_color; text.setColor(color); };
-		inline void setSize(unsigned __int8 new_size) { size = new_size; text.setCharacterSize(size); };
+		inline void setText(std::string new_msg) { text->setString(new_msg); };
+		inline void setColor(sf::Color new_color) { text->setColor(new_color); };
+		inline void setSize(unsigned __int8 new_size) { text->setCharacterSize(new_size); };
+		inline void setPosition(sf::Vector2f new_position) { text->setPosition(new_position); };
 
-		void setSortIndex(unsigned __int8 new_z_index) override { ZIndex = new_z_index; };
-		unsigned __int8 getSortIndex() const override { return ZIndex; };
+		void setSortIndex(unsigned __int8 new_z_index) override { sortIndex = new_z_index; };
+		unsigned __int8 getSortIndex() const override { return sortIndex; };
 		inline void setVisible(bool state) override { isVisible = state; };
 		inline bool getVisible() const override { return isVisible; };
 		inline void setHandleEvent(std::function<void()> new_handler) {};
 		inline void handleEvent(sf::Event& event, sf::RenderWindow& window) override {};
-		inline void render(sf::RenderWindow& window) const override { window.draw(text); };
+		inline void render(sf::RenderWindow& window) const override { window.draw(*text); };
 
 	private:
-		std::string message;
+		std::unique_ptr<sf::Text> text = std::make_unique<sf::Text>();
 		const sf::Font& font;
-		unsigned __int8 size;
-		sf::Text text;
-		sf::Color color;
 		bool isVisible = true;
-		unsigned __int8 ZIndex = 0;
+		unsigned __int8 sortIndex = 0;
 };
