24623c842373bf4ed177393d31b0d4ba5b0bad08
levalyukov <lev.alyukov@gmail.com>
2025-09-29 11:03:16 +0500

    minor changes in inventory-manager

--- src/mechanics/inventory-manager.hpp
+++ src/mechanics/inventory-manager.hpp
@@ -17,15 +17,16 @@ class InventoryManager {
 		};
 		std::unordered_map<std::string, Item> inventory;
 
-		inline void addItem(std::string name, struct Item item) {
-			std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) { return std::tolower(c); });
-			if (getItem(name) == nullptr) {
+		inline void addItem(const std::string name, struct Item item) {
+			std::string lower_name = name;
+			std::transform(lower_name.begin(), lower_name.end(), lower_name.begin(), [](unsigned char c) { return std::tolower(c); });
+			if (getItem(lower_name) == nullptr) {
 				if (inventory.size() < MAX_SLOTS)
-					inventory.emplace(name, item);
-			} else addItemValue(name, 1);
+					inventory.emplace(lower_name, item);
+			} else addItemValue(lower_name, 1);
 		};
 
-		inline Item* getItem(const std::string& name) {
+		inline Item* getItem(const std::string name) {
 			std::string lower_name = name;
 			std::transform(lower_name.begin(), lower_name.end(), lower_name.begin(), [](unsigned char c) { return std::tolower(c); });
 			auto item = inventory.find(lower_name);
@@ -41,16 +42,12 @@ class InventoryManager {
 
 		inline void setItemValue(std::string name, unsigned __int8 value = 0) {
 			std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) {return std::tolower(c); });
-			if (getItem(name) != nullptr) {
-				getItem(name)->value = value;
-			};
+			if (getItem(name) != nullptr) getItem(name)->value = value;
 		};
 
 		inline void addItemValue(std::string name, unsigned __int8 value = 0) {
 			std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) {return std::tolower(c); });
-			if (getItem(name) != nullptr) {
-				getItem(name)->value += value;
-			};
+			if (getItem(name) != nullptr) getItem(name)->value += value;
 		};
 
 		inline void subtractItemValue(std::string name, unsigned __int8 value = 0) {
