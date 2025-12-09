8d4e8724dc75dbffce999f6d83b0228f6a249b6b
levalyukov <lev.alyukov@gmail.com>
2025-12-09 21:27:18 +0500

    Update fridge-manager.hpp

--- src/mechanics/fridge-manager.hpp
+++ src/mechanics/fridge-manager.hpp
@@ -3,39 +3,30 @@
 #include <map>
 #include <string>
 #include <algorithm>
+#include <stdint.h>
 
 class FridgeManager {
 	public:
 		struct Item {
-			std::string title = "";
-			const std::string icon = "";
-			unsigned __int8 value = 0;
+			std::wstring title;
+			const std::string icon;
+			uint8_t value = 0;
 		};
 
-		std::map<std::string, Item> storage;
+		std::map<uint32_t, Item> storage;
 
-		inline void addItem(const std::string name, const struct Item product) {
-			std::string lower_name = name;
-			std::transform(lower_name.begin(), lower_name.end(), lower_name.begin(), 
-				[](unsigned char c) {return std::tolower(c); });
-			if (getItem(lower_name) == nullptr) {
-				storage.emplace(lower_name, product);
-			} else getItem(lower_name)->value += 1;
+		inline void addItem(const uint32_t id, const struct Item product) {
+			if (getItem(id) == nullptr) storage.emplace(id, product); 
+			else getItem(id)->value += 1;
 		};
 
-		inline Item* getItem(const std::string name) {
-			std::string lower_name = name;
-			std::transform(lower_name.begin(), lower_name.end(), lower_name.begin(), 
-				[](unsigned char c) {return std::tolower(c); });
-			auto item = storage.find(lower_name);
+		inline Item* getItem(const uint32_t id) {
+			auto item = storage.find(id);
 			return (item != storage.end()) ? &item->second : nullptr;
 		};
 
-		inline void removeItem(const std::string name) {
-			std::string lower_name = name;
-			std::transform(lower_name.begin(), lower_name.end(), lower_name.begin(),
-				[](unsigned char c) {return std::tolower(c); });
-			auto item = storage.find(lower_name);
+		inline void removeItem(const uint32_t id) {
+			auto item = storage.find(id);
 			if (item != storage.end()) storage.erase(item);
 		};
 };
