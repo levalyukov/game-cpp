084af3a165fc661122fcd82d1b6d7a6593993a41
levalyukov <lev.alyukov@gmail.com>
2026-01-26 21:44:28 +0500

    add event-manager

---
+++ docs/dev/event-manager.md
@@ -0,0 +1,70 @@
+﻿# Класс `EventManager`
+
+Позволяет выполнять лямбды-функции через заданное время.
+
+### Структура `Event_t`
+
+```cpp
+struct Event_t {
+  std::function<void()> lambda;
+  uint32_t frequency;
+  bool repeat = false;
+  uint32_t time = 0;
+  bool performed = false;
+};
+```
+
+- `lambda` - лямбда-функция, которая будет выполнена по достижение время;
+- `frequency` - частота, раз сколько времени будет выполняться событие;
+- `repeat` - флаг, повторяется ли событие или нет;
+- `time` - счетчик времени, по достижение 0 - лямбда-функция выполняется;
+- `performed ` - флаг, отмечает выполнилось ли событие или нет.
+
+Конструктор функции-члена: 
+```cpp
+Event_t(std::function<void()> _lambda, uint32_t _frequency, bool _repeat);
+```
+
+### Методы
+
+Добавление события. Возвращает `true` при успешном добавлении.
+```cpp
+bool addEvent(const std::string& name, std::unique_ptr<Event_t> event);
+```
+
+Возвращает сырой указатель на событие, при его отсутствии возвращает `nullptr`.
+```cpp
+Event_t* getEvent(const std::string& name);
+```
+
+Удаляет событие. Возвращает `true` при успешном удалении.
+```cpp
+bool removeEvent(const std::string& name);
+```
+
+Обработка событий, вызывается каждый кадр.
+```cpp
+void update(void);
+```
+
+### Пример кода
+
+```cpp
+std::unique_ptr<EventManager> events = std::make_unique<EventManager>();
+
+void Game::init(void) {
+  if (events) {
+    events->addEvent("event1", std::make_unique<Event_t>(
+    []() {std::cout << "- Hello!\n\t" << std::endl; }, 1, true));
+  };
+};
+```
+
+Результат:
+```text
+- Hello!
+
+- Hello!
+
+- Hello!
+```
---
+++ src/engine/events/event-manager.hpp
@@ -0,0 +1,92 @@
+﻿#pragma once
+#ifndef ENGINE_EVENT_MANAGER_H
+#define ENGINE_EVENT_MANAGER_H
+
+#include <map>
+#include <cmath>
+#include <stdint.h>
+#include <algorithm>
+#include <functional>
+#include <SFML/Graphics.hpp>
+
+struct Event_t {
+  std::function<void()> lambda;
+  uint32_t frequency;
+  bool repeat = false;
+  uint32_t time = 0;
+  bool performed = false;
+
+  Event_t() = default;
+  Event_t(
+    std::function<void()> _lambda,
+    uint32_t _frequency, 
+    bool _repeat
+  ) : lambda(_lambda), 
+      frequency(_frequency),
+      repeat(_repeat) {};
+};
+
+class EventManager {
+  public:
+    inline bool addEvent(const std::string& name, std::unique_ptr<Event_t> event) {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      if (!getEvent(copy)) {
+        event->time = event->frequency + (delay-clock->getElapsedTime().asSeconds());
+        events.emplace(copy, std::move(event));
+        return true;
+      }; return false;
+    };
+
+    inline Event_t* getEvent(const std::string& name) {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      auto event = events.find(copy);
+      return (event != events.end()) ? event->second.get() : nullptr;
+    };
+
+    inline bool removeEvent(const std::string& name) {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      auto event = events.find(copy);
+      if (event != events.end()) {
+        events.erase(event);
+        return true;
+      }; return false;
+    };
+
+    inline void update(void) {
+      if (events.empty()) return;
+      if (!clock) return;
+
+      if (clock->getElapsedTime().asSeconds() >= delay) {
+        for (auto& event : events) {
+          if (event.second->time == 0) {
+
+            if (!event.second->performed) {
+              event.second->lambda();
+              event.second->performed = true;
+            };
+
+            if (event.second->repeat) {
+              event.second->performed = false;
+              event.second->time = event.second->frequency;
+            } else event.second->performed = true;
+
+          }; event.second->time--;
+        }; delay++;
+      };
+    };
+  private:
+    uint64_t delay = 0;
+    std::map<std::string, std::unique_ptr<Event_t>> events;
+    std::unique_ptr<sf::Clock> clock = std::make_unique<sf::Clock>();
+};
+
+#endif
