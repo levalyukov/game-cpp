e5d410632146352f95e20f6b75cd76c337fdc285
levalyukov <lev.alyukov@gmail.com>
2026-01-26 21:45:17 +0500

    add fonts and textures container and func in uimanager

--- src/engine/ui/ui-manager.hpp
+++ src/engine/ui/ui-manager.hpp
@@ -10,12 +10,14 @@
 
 class UIManager {
 	public:
+
+    /* -- UI Elements -- */
     inline bool addElement(const std::string& name, std::unique_ptr<UIElement> element) {
       if (!element) return false;
 
       std::string copy = name;
       std::transform(copy.begin(), copy.end(), copy.begin(),
-      [](unsigned char c) {return std::tolower(c); });
+        [](unsigned char c) {return std::tolower(c); });
       
       if (!getElement(copy)) {
         elementsSort.emplace_back(std::move(element.get()));
@@ -24,10 +26,10 @@ class UIManager {
       }; return false;
     };
 
-    inline UIElement* getElement(const std::string& name) {
+    inline UIElement* getElement(const std::string& name) const {
       std::string copy = name;
       std::transform(copy.begin(), copy.end(), copy.begin(),
-      [](unsigned char c) {return std::tolower(c); });
+        [](unsigned char c) {return std::tolower(c); });
 
       auto element = elements.find(copy);
       return (element != elements.end()) ? element->second.get() : nullptr;
@@ -60,9 +62,85 @@ class UIManager {
         if (element.second) element.second->update(window, event);
       };
     };
+    /* ----------------- */
+
+    /* -- Fonts -- */
+    inline bool addFont(const std::string& name, const std::string& path_to_font) {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      if (!getFont(copy)) {
+        std::unique_ptr<sf::Font> font = std::make_unique<sf::Font>();
+        if (!font->loadFromFile(path_to_font)) return false;
+        fonts.emplace(copy, std::move(font));
+        return true;
+      }; return false;
+    };
+
+    inline sf::Font* getFont(const std::string& name) const {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      auto font = fonts.find(copy);
+      return (font != fonts.end()) ? font->second.get() : nullptr;
+    };
+
+    inline bool removeFont(const std::string& name) {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      auto font = fonts.find(copy);
+      if (font != fonts.end()) {
+        fonts.erase(font);
+        return true;
+      }; return false;
+    };
+
+    /* ----------- */
+
+    /* -- Textures -- */
+    inline bool addTexture(const std::string& name, const std::string& path_to_texture) {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      if (!getTexture(copy)) {
+        std::unique_ptr<sf::Texture> texture = std::make_unique<sf::Texture>();
+        if (!texture->loadFromFile(path_to_texture)) return false;
+        textures.emplace(copy, std::move(texture));
+        return true;
+      }; return false;
+    };
+
+    inline sf::Texture* getTexture(const std::string& name) const {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      auto texture = textures.find(copy);
+      return texture != textures.end() ? texture->second.get() : nullptr;
+    };
+
+    inline bool removeTexture(const std::string& name) {
+      std::string copy = name;
+      std::transform(copy.begin(), copy.end(), copy.begin(),
+        [](unsigned char c) {return std::tolower(c); });
+
+      auto texture = textures.find(copy);
+      if (texture != textures.end()) {
+        textures.erase(copy);
+        return true;
+      }; return false;
+    };
+
   private:
     std::vector<UIElement*> elementsSort;
     std::map<std::string, std::unique_ptr<UIElement>> elements;
+    std::map<std::string, std::unique_ptr<sf::Font>> fonts;
+    std::map<std::string, std::unique_ptr<sf::Texture>> textures;
 };
 
 #endif 
