a87830de72a1a9f4ce3c4f0fc9beb85a9e20052a
levalyukov <lev.alyukov@gmail.com>
2026-02-02 19:09:30 +0500

    update label

--- src/engine/ui/elements/label.hpp
+++ src/engine/ui/elements/label.hpp
@@ -8,7 +8,7 @@ class Label : public UIElement {
     Label(
       const std::wstring& message,
       sf::Font* label_font,
-      const uint16_t size,
+      const uint16_t size = 20,
       const sf::Color& color = sf::Color::White
     ) : font(*label_font) {
       if (!text) text = std::make_unique<sf::Text>();
@@ -17,7 +17,7 @@ class Label : public UIElement {
       text->setCharacterSize(size);
     };
 
-    inline sf::Text& getElement(void) const { return *text.get(); };
+    inline sf::Text& getLabel(void) const { return *text.get(); };
     inline void update(sf::RenderWindow& window, sf::Event& event) { window.draw(*text); };
     inline void setDepth(const int16_t new_depth) { depth = new_depth; };
     inline int16_t getDepth(void) const { return depth; };
